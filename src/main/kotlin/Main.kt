cec6975dfdf4d00156965f1a69f76719763b9304
ckainz11 <christoph.kainz11@gmail.com>
2021-11-03 12:35:22 +0100

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    println("File Output summed:")
+    println(getResourceAsInts("file.txt").sum())
+    if(args.get(0) == "1"){
+        //TODO
+    }
+    else if(args.get(0) == "2"){
+        //TODO
+    }
+    else if(args.get(0) == "3"){
+        //TODO
+    }
+    else if(args.get(0) == "4"){
+        //TODO
+    }
+    else if(args.get(0) == "5"){
+        //TODO
+    }
+    else if(args.get(0) == "6"){
+        //TODO
+    }
+    else if(args.get(0) == "7"){
+        //TODO
+    }
+}
---
+++ src/main/kotlin/Util.kt
@@ -0,0 +1,17 @@
+val anon = object {}
+
+fun getResource(name: String) = anon.javaClass.getResource(name)
+
+fun getResourceAsLines(name: String) = getResourceAsString(name).lines()
+fun getResourceAsString(name: String) = getResource(name).readText()
+
+
+fun getResourceAsInts(name: String) = getResourceAsLines(name).map(String::toInt)
+
+
+class NoSolutionFoundException : Exception {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
