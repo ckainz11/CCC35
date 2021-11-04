f8498907122700949132ab9accf1ac52e868669e
Florian Flatscher <fla19488@spengergasse.at>
2021-11-04 18:31:33 +0100

    progress

--- src/main/kotlin/level1/Level.kt
+++ src/main/kotlin/level1/Level.kt
@@ -1,7 +1,23 @@
 package level1
 
+data class ActionPair (val move: String, val count: Int,)
+
 fun solve(input: String): String {
     val lines = input.split(Regex("\r?\n"))
-    val data = lines.map { it.toInt() }
-    return data.sum().toString()
+    val datalines = lines.map {
+        var l = it.split(" ")
+        l = l.subList(1, l.size)
+        val dataline = mutableListOf<ActionPair>()
+        for (i in 0 until l.size-1 step 2) {
+            dataline.add(ActionPair(l[i], l[i+1].toInt()))
+        }
+        dataline
+    }
+
+
+    val x =
+        datalines.map {
+            it.sumOf { it.move.count { it == 'F' } * it.count }
+        }.joinToString("\n")
+    return x
 }
