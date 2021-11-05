76d4efeb4aeaa05c0419ee96184e67c784e939d4
Florian Flatscher <florian.flatscher@gmail.com>
2021-11-05 11:37:08 +0100

    progress

--- src/main/kotlin/level2/Level.kt
+++ src/main/kotlin/level2/Level.kt
@@ -9,26 +9,27 @@ data class Basket(val values: List<Int>) {
     fun getValueSum(): Int {
         return values.sum();
     }
+
     fun max(): Int {
         return values.maxOrNull() ?: 0
     }
 }
 
-data class Trade(val with:Economist, val weGive:Int, val weGet:Int)
+data class Trade(val with: Economist, val weGive: Int, val weGet: Int)
 
 fun solve(input: String): String {
 
     val lines = input.trimEnd('\n').split(Regex("\r?\n"))
     val numE = lines[0].toInt()
-    val economists = lines.subList(1, numE+1).map {
+    val economists = lines.subList(1, numE + 1).map {
         it.split(" ").map {
             it.toInt()
         }
     }.map {
         Economist(Basket(it.subList(1, it.size)), it[0])
     }
-    val numS = lines[numE+1].toInt()
-    lines.subList(numE+2, lines.size).forEach {
+    val numS = lines[numE + 1].toInt()
+    lines.subList(numE + 2, lines.size).forEach {
         val data = it.split(' ')
         for ((index, value) in data.subList(1, data.size).withIndex()) {
             economists[index].preferences.add(value.toInt())
@@ -37,8 +38,8 @@ fun solve(input: String): String {
 
     var output = "";
     for ((i, we) in economists.withIndex()) {
-        val weGive = we.basket.values.minByOrNull {
-            we.preferences[it-1]
+        val weGiveALl = we.basket.values.sortedBy {
+            we.preferences[it - 1]
         }!!
         var found = false
         var currentBestTrade: Trade? = null
@@ -48,16 +49,32 @@ fun solve(input: String): String {
                 continue
             }
             val otherAllWeWant = other.basket.values.sortedByDescending {
-                we.preferences[it-1]
+                we.preferences[it - 1]
             }
-            for (otherGive in otherAllWeWant) {
-                if (other.preferences[weGive-1] > other.preferences[otherGive-1]) {
-                    val wouldBe = we.preferences[otherGive-1] - we.preferences[weGive-1]
-                    if (wouldBe > currentBestTradeValue) {
-                        currentBestTrade = Trade(other, weGive, otherGive)
-                        currentBestTradeValue = wouldBe
+            for (weGive in weGiveALl) {
+                for (otherGive in otherAllWeWant) {
+                    if (other.preferences[weGive - 1] > other.preferences[otherGive - 1]) {
+                        val wouldBe = we.preferences[otherGive - 1] - we.preferences[weGive - 1]
+                        if (wouldBe > currentBestTradeValue) {
+                            currentBestTrade = Trade(other, weGive, otherGive)
+                            currentBestTradeValue = wouldBe
+                        } else if (wouldBe == currentBestTradeValue && currentBestTrade != null) {
+                            if (currentBestTrade.with.id > other.id) {
+                                currentBestTrade = Trade(other, weGive, otherGive)
+                                currentBestTradeValue = wouldBe
+                            } else if (currentBestTrade.with.id == other.id) {
+                                if (currentBestTrade.weGive > weGive) {
+                                    currentBestTrade = Trade(other, weGive, otherGive)
+                                    currentBestTradeValue = wouldBe
+                                } else if (currentBestTrade.weGive == weGive) {
+                                    if (currentBestTrade.weGet > otherGive) {
+                                        currentBestTrade = Trade(other, weGive, otherGive)
+                                        currentBestTradeValue = wouldBe
+                                    }
+                                }
+                            }
+                        }
                     }
-                    break
                 }
             }
         }
